591624a50e6d4e7819ba06383ad75ee68d3de776
Marc Bakker <marc.bakker@eonics.nl>
2024-03-07 17:09:00 +0100

    toevoegen componenten ContentPane,TextField,ContentHeader

--- libs/whatschat/ui-components/src/lib/whatschat-ui-components.tsx
+++ libs/whatschat/ui-components/src/lib/whatschat-ui-components.tsx
@@ -1,15 +1,17 @@
 import { PropsWithChildren } from 'react'
 import styles from './whatschat-ui-components.module.scss';
+import TextField from '@mui/material/TextField';
 
 import Stack from '@mui/material/Stack';
 import React from 'react';
 
-// Stack
+// LeftPane
 type LeftPaneProps = {children: JSX.Element[]}
 
 export function LeftPane({children}: LeftPaneProps) {
   return <Stack className={styles.leftPane}>{children}</Stack>
 }
+
 
 // Avatar 
 type AvatarProps = {url:string, alt:string}
@@ -20,6 +22,7 @@ export function Avatar(props: AvatarProps) {
       <img className={styles.avatar} src={url} alt={alt}></img>
   );
 }
+
 
 // Balloon in left pane
 type BalloonProps = {id: number, title: string, isSelected: boolean, selectBalloon: (id: number) => void}
@@ -35,5 +38,32 @@ export function Balloon(props: PropsWithChildren<BalloonProps>) {
       >
     <div className={styles.balloon__avatar}>{children}</div>
     <div className={styles.balloon__title}>{title}</div>
+    </div>)
+}
+
+
+// ContentPane
+export function ContentPane() {
+  return (
+    <div className={styles.contentPane}>
+    </div>
+  ) 
+}
+
+
+// TextField
+export function TextFieldContainer() {
+  return (<div className={styles.textField}><TextField fullWidth variant="filled"/></div>)
+}
+
+
+// ContentHeader
+type contentHeaderProps = {title: string};
+
+export function ContentHeader(props: contentHeaderProps){
+  const {title} = props
+  return (
+    <div className={styles.contentHeader}>
+      <div className={styles.contentTitle}>{title}</div>
     </div>)
 }
