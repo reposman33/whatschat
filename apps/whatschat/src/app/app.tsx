5e9dea2004c5d69c8ff27f783d1a993aef344d74
Marc Bakker <marc.bakker@eonics.nl>
2024-03-07 12:52:03 +0100

    haal dummy userdata op met axios.

--- apps/whatschat/src/app/app.tsx
+++ apps/whatschat/src/app/app.tsx
@@ -3,23 +3,31 @@ import styles from './app.module.scss';
 import { LeftPane } from '@ui-components';
 import { Balloon }from '@ui-components';
 import { Avatar }from '@ui-components';
+import axios from 'axios';
+import React from 'react';
 
-type User = {url: string, alt: string, title: string};
-const users: User[] = [
-  {alt: 'Marc met hoed', title: 'Ballon 1',  url:'/img/marc_met_hoed.jpg'},
-  {alt: 'Marc met hoed', title: 'Ballon 2',  url:'/img/marc_met_hoed.jpg'},
-  {alt: 'Marc met hoed', title: 'Ballon 3',  url:'/img/marc_met_hoed.jpg'},
-  {alt: 'Marc met hoed', title: 'Ballon 4',  url:'/img/marc_met_hoed.jpg'},
-  {alt: 'Marc met hoed', title: 'Ballon ',  url:'/img/marc_met_hoed.jpg'},
-]
+type User = {id: number, url: string, alt: string, title: string};
+const baseURL = 'http://localhost:4200/public/api/users.json';
 
 export function App() {
-  const balloons = users.map((user: User) => 
-    <Balloon title={user.title}><Avatar url={user.url} alt={user.alt}></Avatar></Balloon>
-  );
+  const [users, setUsers] = React.useState([])
+  let balloons: JSX.Element[] = [];
+
+  React.useEffect(()=>{
+    axios.get(baseURL)
+    .then(({data}) => 
+      setUsers(data))
+  },[]);
+
+    balloons = users
+    ? users.map((user: User) => 
+      <Balloon key={user.id} title={user.title}><Avatar url={user.url} alt={user.alt}></Avatar></Balloon>
+    )
+    : [];
+
   return (
     <LeftPane>
-      {balloons}
+      {balloons || []}
     </LeftPane>
   );
 }
